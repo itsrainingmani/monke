7de2d4231f97277e677f19283c221852fb378f8f
Manikandan Sundararajan <mani@hey.com>
2021-04-14 22:26:23 -0400

    Function to get the next_token

--- monke/lexer.py
+++ monke/lexer.py
@@ -1,5 +1,6 @@
 from ast import Str
 from dataclasses import dataclass
+from monke import token
 
 # Source Code -> Tokens -> AST
 
@@ -19,6 +20,31 @@ class Lexer:
 
         self.position = self.read_position
         self.read_position += 1
+
+    def next_token(self) -> token.Token:
+        ch = self.ch
+
+        if ch == "=":
+            tok = token.new_token(token.ASSIGN, ch)
+        elif ch == ";":
+            tok = token.new_token(token.SEMICOLON, ch)
+        elif ch == "(":
+            tok = token.new_token(token.LPAREN, ch)
+        elif ch == ")":
+            tok = token.new_token(token.RPAREN, ch)
+        elif ch == ",":
+            tok = token.new_token(token.COMMA, ch)
+        elif ch == "+":
+            tok = token.new_token(token.PLUS, ch)
+        elif ch == "{":
+            tok = token.new_token(token.LBRACE, ch)
+        elif ch == "}":
+            tok = token.new_token(token.RBRACE, ch)
+        elif ch == 0:
+            tok = token.new_token(token.EOF, "")
+
+        self.read_char()
+        return tok
 
 
 def new(input: str) -> Lexer:
