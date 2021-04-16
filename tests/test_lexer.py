5336df23ca215f03f2d3f614e39c129c5b72cf32
Manikandan Sundararajan <mani@hey.com>
2021-04-16 14:51:46 -0400

    Complete expected token set for the third test

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -80,3 +80,76 @@ let result = add(five, ten);"""
         tok = l.next_token()
         # print(tok, t)
         assert tok == t
+
+
+def test_next_token_extended():
+    input = """let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+"""
+
+    expected = [
+        token.Token(token.LET, "let"),
+        token.Token(token.IDENT, "five"),
+        token.Token(token.ASSIGN, "="),
+        token.Token(token.INT, "5"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.LET, "let"),
+        token.Token(token.IDENT, "ten"),
+        token.Token(token.ASSIGN, "="),
+        token.Token(token.INT, "10"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.LET, "let"),
+        token.Token(token.IDENT, "add"),
+        token.Token(token.ASSIGN, "="),
+        token.Token(token.FUNCTION, "fn"),
+        token.Token(token.LPAREN, "("),
+        token.Token(token.IDENT, "x"),
+        token.Token(token.COMMA, ","),
+        token.Token(token.IDENT, "y"),
+        token.Token(token.RPAREN, ")"),
+        token.Token(token.LBRACE, "{"),
+        token.Token(token.IDENT, "x"),
+        token.Token(token.PLUS, "+"),
+        token.Token(token.IDENT, "y"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.RBRACE, "}"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.LET, "let"),
+        token.Token(token.IDENT, "result"),
+        token.Token(token.ASSIGN, "="),
+        token.Token(token.IDENT, "add"),
+        token.Token(token.LPAREN, "("),
+        token.Token(token.IDENT, "five"),
+        token.Token(token.COMMA, ","),
+        token.Token(token.IDENT, "ten"),
+        token.Token(token.RPAREN, ")"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.BANG, "!"),
+        token.Token(token.MINUS, "-"),
+        token.Token(token.SLASH, "/"),
+        token.Token(token.ASTERISK, "*"),
+        token.Token(token.INT, "5"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.INT, "5"),
+        token.Token(token.LT, "<"),
+        token.Token(token.INT, "10"),
+        token.Token(token.GT, ">"),
+        token.Token(token.INT, "5"),
+        token.Token(token.SEMICOLON, ";"),
+        token.Token(token.EOF, ""),
+    ]
+
+    l = lexer.new(input)
+
+    for t in expected:
+        tok = l.next_token()
+        # print(tok, t)
+        assert tok == t
